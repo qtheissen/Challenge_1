01930d9ad2669dee4facc1a4b89df8ef33a4ffb4
qtheissen <325957@student.mboutrecht.nl>
2019-11-08 10:55:53 +0100

    Initial commit

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+
+    //Private Vars
+    private float propellerSpeed = 600;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * propellerSpeed);
+    }
+}
